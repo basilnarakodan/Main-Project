3e79f79540041ea1af73fbe1bcc1baff1e322ede
basilnarakodan <basilnarakodan@gmail.com>
2023-03-12 13:46:21 +0530

    signin completed

--- foodelivery/src/navigators/index.js
+++ foodelivery/src/navigators/index.js
@@ -32,21 +32,20 @@ const Navigators = () => {
             <Stack.Navigator screenOptions={{ headerShown: false }}>
                 {
                     isAppLoading ? (<Stack.Screen name="Splash" component={SplashScreen} />
-                    ) :
-                        !token ? (
-                            <>
-                                {isFirstTimeUse && (
-                                    <Stack.Screen name="Welcome" component={WelcomeScreen} />
-                                )}
-                                <Stack.Screen name="Signin" component={SigninScreen} />
-                                <Stack.Screen name="Signup" component={SignupScreen} />
-                                <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
-                                <Stack.Screen name="RegisterPhone" component={RegisterPhoneScreen} />
-                                <Stack.Screen name="Verification" component={VerificationScreen} />
-                            </>
-                        ) : (
-                            <Stack.Screen name="Home" component={HomeScreen} />
-                        )}
+                    ) : !token || token === null || token === '' ? (
+                        <>
+                            {isFirstTimeUse && (
+                                <Stack.Screen name="Welcome" component={WelcomeScreen} />
+                            )}
+                            <Stack.Screen name="Signin" component={SigninScreen} />
+                            <Stack.Screen name="Signup" component={SignupScreen} />
+                            <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+                            <Stack.Screen name="RegisterPhone" component={RegisterPhoneScreen} />
+                            <Stack.Screen name="Verification" component={VerificationScreen} />
+                        </>
+                    ) : (
+                        <Stack.Screen name="Home" component={HomeScreen} />
+                    )}
             </Stack.Navigator>
         </NavigationContainer>
 
